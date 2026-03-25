4c6fc52a79becacb423490471576aab168c04b6f
Moiz-205 <moizrajput205@gmail.com>
2026-03-25 21:39:30 +0500

    clock updated

--- clock/clock.py
+++ clock/clock.py
@@ -1,25 +1,28 @@
 import time
 
 
-def time_inc(s: int, m: int, h: int):
-    if s == 4:
-        m += 1
-        s = 0
-    if m == 4:
-        h += 1
-        m = 0
-    return s, m, h
-
+def timer(sec: int, min: int, hr: int):
+    if sec == 60:
+        min += 1
+        sec = 0
+    elif min == 60:
+        hr += 1
+        min = 0
+    elif hr == 24:
+        hr = 0
+        min = 0
+        sec = 0
+    return sec, min, hr
 
 def clock() -> None:
-    s = 0
-    m = 0
-    h = 0
+    sec = 0
+    min = 0
+    hr = 0
     while True:
-        print(f"\r{h:02} : {m:02} : {s:02}", end="", flush=True)
+        print(f"\r{hr:02} : {min:02} : {sec:02}", end="", flush=True)
         time.sleep(1)
-        s += 1
-        s, m, h = time_inc(s, m, h)
+        sec += 1
+        sec, min, hr = timer(sec, min, hr)
 
 
 def main() -> None:
