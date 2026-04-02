447b620943b1842767a40c780d8667b074362b6a
Moiz-205 <moizrajput205@gmail.com>
2026-04-03 01:57:33 +0500

    function for save to text file

--- ascii-art/main.py
+++ ascii-art/main.py
@@ -1,29 +1,47 @@
 from PIL import Image
+import sys
+import os
 
-def load_image(path):
-    im = Image.open(path)
-    width, height = im.size
-    im = im.convert("L")
-    new_width = 50
+def load_image(path, new_width=100):
+    path = "images/" + path
+    image = Image.open(path)
+    image = image.convert("L")
+    width, height = image.size
     new_height = int((height / width) * new_width * 0.5)
-    im = im.resize((new_width, new_height))
-    return im, new_width, new_height
+    image = image.resize((new_width, new_height))
+    return image, new_width, new_height
 
 def to_ascii(image, width, height):
-    row = ""
     chars = "@%#&-=+_:.,$!*"
+    art = ""
     for y in range(height):
         row = ""
         for x in range(width):
             pixel = image.getpixel((x, y))
             index = int(pixel) * (len(chars) - 1) // 255    # type: ignore
             row += chars[index]
-        print(row)
+        art += row + "\n"
+    return art
+
+def save_ascii(rows, path):
+    name = os.path.splitext(path)[0]
+    output_filename = "ascii-" + name + ".txt"
+    output_path = os.path.join("outputs", output_filename)
+    with open(output_path, "w") as f:
+        f.write(rows)
 
 def main():
-    path = "images/car.png"
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <image_path>")
+        sys.exit(1)
+    path = sys.argv[1]
     image, width, height = load_image(path)
-    to_ascii(image, width, height)
+    art = to_ascii(image, width, height)
+    user = int(input("1. Save to file \t 2. Print to terminal\nChoice: "))
+    if user == 1:
+        save_ascii(art, path)
+    elif user == 2:
+        print(art)
 
 if __name__ == "__main__":
     main()
