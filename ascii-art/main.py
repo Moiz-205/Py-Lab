2ef799d020ce85e99320b8841968879aaa2a4e33
Moiz-205 <moizrajput205@gmail.com>
2026-04-03 00:41:45 +0500

    refactored for functions

--- ascii-art/main.py
+++ ascii-art/main.py
@@ -1,23 +1,29 @@
 from PIL import Image
 
-path = "images/car.png"
-chars = "@%#&-=+_:.,$!*"
-row = ""
-
-with Image.open(path) as im:
-    im = im.convert("L")
+def load_image(path):
+    im = Image.open(path)
     width, height = im.size
-    print(width, height)
-    print(im.mode)
+    im = im.convert("L")
     new_width = 50
     new_height = int((height / width) * new_width * 0.5)
     im = im.resize((new_width, new_height))
-    print(im.size)
+    return im, new_width, new_height
 
-for y in range(new_height):
+def to_ascii(image, width, height):
     row = ""
-    for x in range(new_width):
-        pixel = im.getpixel((x, y))
-        index = int(pixel * (len(chars) - 1) // 255)
-        row += chars[index]
-    print(row)
+    chars = "@%#&-=+_:.,$!*"
+    for y in range(height):
+        row = ""
+        for x in range(width):
+            pixel = image.getpixel((x, y))
+            index = int(pixel) * (len(chars) - 1) // 255    # type: ignore
+            row += chars[index]
+        print(row)
+
+def main():
+    path = "images/car.png"
+    image, width, height = load_image(path)
+    to_ascii(image, width, height)
+
+if __name__ == "__main__":
+    main()
