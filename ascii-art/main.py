48aeea95df9f5d8246fb69e184a11da3c4ed5acd
Moiz-205 <moizrajput205@gmail.com>
2026-04-03 02:51:36 +0500

    modified for colored ascii art function

--- ascii-art/main.py
+++ ascii-art/main.py
@@ -2,46 +2,63 @@ from PIL import Image
 import sys
 import os
 
-def load_image(path, new_width=100):
+def load_image(path, color: bool, new_width=100):
     path = "images/" + path
     image = Image.open(path)
-    image = image.convert("L")
+    if color:
+        image = image.convert("RGB")
+    else:
+        image = image.convert("L")
     width, height = image.size
     new_height = int((height / width) * new_width * 0.5)
     image = image.resize((new_width, new_height))
     return image, new_width, new_height
 
-def to_ascii(image, width, height):
+def to_ascii(image, width, height, color):
     chars = "@%#&-=+_:.,$!*"
     art = ""
-    for y in range(height):
-        row = ""
-        for x in range(width):
-            pixel = image.getpixel((x, y))
-            index = int(pixel) * (len(chars) - 1) // 255    # type: ignore
-            row += chars[index]
-        art += row + "\n"
+
+    if color:
+        for y in range(height):
+            row = ""
+            for x in range(width):
+                r, g, b = image.getpixel((x, y))
+                brightness = int(0.299*r + 0.587*g + 0.114*b)
+                index = brightness * (len(chars) - 1) // 255
+                row += f"\033[38;2;{r};{g};{b}m{chars[index]}\033[0m"   # standard luminance formula
+            art += row + "\n"
+    else:
+        for y in range(height):
+            row = ""
+            for x in range(width):
+                pixel = image.getpixel((x, y))
+                index = int(pixel) * (len(chars) - 1) // 255    # type: ignore
+                row += chars[index]
+            art += row + "\n"
     return art
 
-def save_ascii(rows, path):
+def save_ascii(art, path):
     name = os.path.splitext(path)[0]
     output_filename = "ascii-" + name + ".txt"
     output_path = os.path.join("outputs", output_filename)
+    print(f"File saved at location: {output_path}")
     with open(output_path, "w") as f:
-        f.write(rows)
+        f.write(art)
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: python main.py <image_path>")
         sys.exit(1)
     path = sys.argv[1]
-    image, width, height = load_image(path)
-    art = to_ascii(image, width, height)
-    user = int(input("1. Save to file \t 2. Print to terminal\nChoice: "))
-    if user == 1:
+    color = input("Want color ascii art (y/n): ").strip().lower() == 'y'
+    image, width, height = load_image(path, color)
+    art = to_ascii(image, width, height, color)
+    print(art)
+    save = input("Want to save ascii art (y/n) :").strip().lower() == 'y'
+    if save:
         save_ascii(art, path)
-    elif user == 2:
-        print(art)
+    else:
+        print("Exited.")
 
 if __name__ == "__main__":
     main()
